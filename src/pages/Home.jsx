ee7c32674a160c7dbe36c662b8be7cadc46d9b5a
Marcelo Mendes <mendes.melo@gmail.com>
2024-08-09 13:28:55 -0300

    Deleted unimportant comments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,3 @@
-// const Home = () => {
-//   return (
-//     <div>
-//       <h1>Bem-vindo à Página de Troca de Livros</h1>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
 import styles from './Home.module.css';
 
 const Home = () => {
