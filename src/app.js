a99bd07ec1428b688246dd23f0517d65e05924a4
Marcelo Mendes <mendes.melo@gmail.com>
2024-08-13 11:04:02 -0300

    Installed CORS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
+// import express from 'express';
+// import routes from './routes/index.js';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const app = express();
+// app.use(express.json());
+// app.use(routes);
+
+// export default app;
+
 import express from 'express';
 import routes from './routes/index.js';
 import dotenv from 'dotenv';
+import cors from 'cors';  // Importe o cors
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// Configuração do CORS
+app.use(cors({
+  origin: 'http://localhost:5173'  // Altere para a origem do seu frontend
+}));
+
 app.use(routes);
 
 export default app;
