453697f662fd1d539051d098002e1b023023cdd0
Marcelo Mendes <mendes.melo@gmail.com>
2024-08-13 13:06:24 -0300

    Deleted unimportant comments

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-// import express from 'express';
-// import routes from './routes/index.js';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const app = express();
-// app.use(express.json());
-// app.use(routes);
-
-// export default app;
-
 import express from 'express';
 import routes from './routes/index.js';
 import dotenv from 'dotenv';
