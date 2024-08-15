cac86fa063a0dbb9e209fb814bc4f145858bc2eb
Riam <riamstefeson616@gmail.com>
2024-08-15 17:49:48 -0300

    Mudando um pouco o estilo do componente BookList.jsx

--- src/pages/BookList/BookList.jsx
+++ src/pages/BookList/BookList.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from './BookList.module.css';
 import Header from '../../components/Header/Header';
+import Title from '../../components/Title/Title';
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
@@ -49,31 +50,42 @@ const BookList = () => {
     <>
       <Header />
       <div className={styles.container}>
-        <input 
-          type="text"
-          placeholder="Buscar por título"
-          value={filters.title}
-          onChange={(e) => handleFilterChange('title', e.target.value)}
-        />
-        <input 
-          type="text"
-          placeholder="Buscar por autor"
-          value={filters.author}
-          onChange={(e) => handleFilterChange('author', e.target.value)}
-        />
-        <input 
-          type="text"
-          placeholder="Buscar por categoria"
-          value={filters.category}
-          onChange={(e) => handleFilterChange('category', e.target.value)}
-        />
+        <div className={styles.container_inputs}>
+          <input 
+            type="text"
+            placeholder="Buscar por título"
+            value={filters.title}
+            onChange={(e) => handleFilterChange('title', e.target.value)}
+            className={styles.input}
+          /> 
+          <input 
+            type="text"
+            placeholder="Buscar por autor"
+            value={filters.author}
+            onChange={(e) => handleFilterChange('author', e.target.value)}
+            className={styles.input}
+          />
+          <input 
+            type="text"
+            placeholder="Buscar por categoria"
+            value={filters.category}
+            onChange={(e) => handleFilterChange('category', e.target.value)}
+            className={styles.input}
+          />
+        </div>
         
         <div className={styles.bookList}>
           {filteredBooks.map((book) => (
             <div key={book.book_id} className={styles.bookItem}>
-              <h3>{book.title}</h3>
-              <p>{book.author}</p>
-              <p>{book.category}</p>
+              <Title level="3">{book.title}</Title>
+              <div>
+                <p className={styles.info}>
+                  Autor: <span className={styles.subInfor}>{book.author}</span>
+                </p>
+                <p className={styles.info}>
+                  Categoria: <span className={styles.subInfor}>{book.category}</span>
+                </p>
+              </div>
             </div>
           ))}
         </div>
