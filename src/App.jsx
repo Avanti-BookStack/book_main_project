67e43bb820d86e6b0f9589d3a9a87aa52f5f054b
Emanuel Pereira <105138144+EmanuelPereiraCruz@users.noreply.github.com>
2024-08-23 16:04:45 -0300

    Update App.jsx
    Importe a tela de Perfil pro App. JSX.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cadastro from './pages/Register/Register';
 import BookList from './pages/BookList/BookList'; // Novo import
 import Footer from './components/Footer/Footer';
 import BookRegister from './pages/BookRegister/BookRegister';
+import PerfilUser from './pages/Perfil/PerfilUser'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/cadastre-se" element={<Cadastro />} />
         <Route path="/buscar-livros" element={<BookList />} />
         <Route path="/cadastrar-livro" element={<BookRegister />} /> 
+        <Route path="/perfil" element={<PerfilUser />} /> 
       </Routes>
       <Footer/>
     </BrowserRouter>
